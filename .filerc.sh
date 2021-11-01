72114f8ebffe9ddb5848324b4506490a41ec6bdc
aidenszeto <aszeto35@gmail.com>
2021-11-01 14:07:35 -0700

    Updated filerc configs

--- .filerc.sh
+++ .filerc.sh
@@ -1,1 +1,12 @@
 cd ~
+
+function mkcd {
+  last=$(eval "echo \$$#")
+  if [ ! -n "$last" ]; then
+    echo "Enter a directory name"
+  elif [ -d $last ]; then
+    echo "\`$last' already exists"
+  else
+    mkdir $@ && cd $last
+  fi
+}
